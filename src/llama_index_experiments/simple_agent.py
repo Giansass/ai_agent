c68fd216269a23e0a0e16220a0f4a1bf52586d4c
Giansanti Andrea (DS&G) <andrea.giansanti@enel.com>
2025-04-28 10:27:17 +0200

    little changed on src structure

--- src/llama_index_experiments/simple_agent.py
+++ src/llama_index_experiments/simple_agent.py
@@ -1,18 +1,19 @@
 """src base module."""
 import os
+
 from dotenv import load_dotenv
+from llama_index.core.agent.workflow import FunctionAgent
+from llama_index.llms.google_genai import GoogleGenAI
 
 load_dotenv()
 
-from llama_index.llms.google_genai import GoogleGenAI
-from llama_index.core.agent.workflow import AgentWorkflow, FunctionAgent
-
-gemini_api_key = os.environ.get('GOOGLE_API_KEY')
+gemini_api_key = os.environ.get("GOOGLE_API_KEY")
 
 llm = GoogleGenAI(
     model="models/gemini-2.0-flash",
     api_key=gemini_api_key,
 )
+
 
 def multiply(a: float, b: float) -> float:
     """Multiply two numbers and returns the product"""
@@ -23,16 +24,21 @@ def add(a: float, b: float) -> float:
     """Add two numbers and returns the sum"""
     return a + b
 
+
 workflow = FunctionAgent(
     tools=[multiply, add],
     llm=llm,
     system_prompt="You are an agent that can perform basic mathematical operations using tools.",
 )
 
+
 async def main():
+    """Main function"""
     response = await workflow.run(user_msg="What is 20+(2*4)?")
     print(response)
 
+
 if __name__ == "__main__":
     import asyncio
+
     asyncio.run(main())
